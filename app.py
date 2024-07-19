fec5f3cbc2eee7f04e74fff0f42877ebe04b196a
Gargi Bhise <gargib.2496@gmail.com>
2024-07-18 22:03:02 -0700

    Bot response for Hello command

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.default()
-intents.messages = True   # This will allow us to send and recieve messages.
+intents.message_content = True   # This will allow us to send and recieve messages.
 
 bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Bot is ready as {bot.user.name}")
+
+@bot.command(name="hello")
+async def hello(ctx):
+    await ctx.send("Hello, I am a Bot!!!")
 
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
 
