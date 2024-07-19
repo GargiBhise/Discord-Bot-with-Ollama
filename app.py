5e399d98c7f5dcf783891e994482a255dea6a8a2
Gargi Bhise <gargib.2496@gmail.com>
2024-07-18 22:19:20 -0700

    Bot response for generic questions

--- app.py
+++ app.py
@@ -21,6 +21,22 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello, I am a Bot!!!")
 
+@bot.command(name="ask")
+async def ask(ctx, *, message):
+    print(message)
+    print("*************************************")
+    response = ollama.chat(model='llama3', messages=[
+         {
+        'role': 'system',
+        'content': 'You are a helpful assistant who provides an answer to questions concisely in no more than 1000 words.'
+       },
+       { 'role': 'user',
+        'content' : message,
+       },
+    ])
+    print(response['message']['content'])
+    await ctx.send(response['message']['content'])
+
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
 
 # response = ollama.chat(model='llama3', messages=[
